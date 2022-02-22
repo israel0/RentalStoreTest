1a7fbc82235fcc52974f8dbd49ba7be952b013c7
israel0 <seyeakinsola@gmail.com>
2022-02-22 01:34:17 +0100

    Add the PostMan Collection

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -45,10 +45,10 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
-                ->middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+            Route::middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
+
 
             Route::middleware('web')
                 ->namespace($this->namespace)
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Spatie\Activitylog\Models\Activity;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {  return $request->user();});
+Route::get('/', function () { return view('/welcome'); });
+Auth::routes();
+Route::get('/log', function () { return Activity::all();   });
+Route::get('/status' , function (){ return $activity  =  Activity::where('description' , 'updated')->get();});
+Route::get('/users' , 'UserController@index')->name('users.index');
+Route::get('/renthistory/{user_id}' , 'UserController@renthistory')->name('renthistory');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/reports', 'HomeController@reports')->name('reports');
+Route::get('/items' ,'ItemController@index')->name('item.index');
+Route::resource('/items', 'ItemController');
